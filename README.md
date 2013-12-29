7cc0e339f9fad03d1bd700d9d1ef6c98738640d1
Marco Faustinelli <contatti@faustinelli.net>
2013-12-29 12:56:16 +0100

    introduced typed racket with assertions

--- README.md
+++ README.md
@@ -4,9 +4,11 @@ These are all studies and experiments with continuations in DrRacket.
 
 It will also probably grow to include studies with streams.
 
-The main advantage of studying with Racket is the possibility to write tests directly together with the executing code.
+The main advantages of studying with Racket are:
+- the possibility to write tests directly together with the executing code
+- the possibility fo follow execution in the stepper (and learn from your own mistakes)
 
-Racket is chosen also because it contains a more or less direct link with Scheme (Little and Seasoned Schemer)
+Racket is chosen also because it contains a direct link with Scheme (see Little and Seasoned Schemer)
 
 OFFICIAL RELEASE
 ---------------- 
--- racketContinuations.rkt
+++ racketContinuations.rkt
@@ -1,6 +1,5 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname racketContinuations) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+#lang typed/racket/no-check
+
 ;(define (atom? x) (not (or (pair? x) (null? x))))
 
 (define sum-list
@@ -16,7 +15,12 @@
 (define doubler (lambda(x)(* 2 x)))
 
 
-(check-expect (sum-list (list 1 3 4 2) doubler) 20)
-(check-expect (sum-list (list  (list 3 4) 1 2) doubler) 20)
-(check-expect (sum-list (list 6 (list 3 (list 4)) 1 (list 1 2 (list 4 (list 1 3 (list (list 1 2) 6)) 2))) doubler) 72)
+;(check-expect (sum-list (list 1 3 4 2) doubler) 20)
+;(check-expect (sum-list (list  (list 3 4) 1 2) doubler) 20)
+;(check-expect (sum-list (list 6 (list 3 (list 4)) 1 (list 1 2 (list 4 (list 1 3 (list (list 1 2) 6)) 2))) doubler) 72)
+
+(define b 12)
+(define a 13)
+
+(assert (< b a))
 
