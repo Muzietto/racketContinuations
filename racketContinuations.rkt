67de5d4a3bb37f93c08df4386f9358a67bd2d301
Marco Faustinelli <contatti@faustinelli.net>
2013-12-29 13:03:15 +0100

    try with call-cc works

--- racketContinuations.rkt
+++ racketContinuations.rkt
@@ -19,8 +19,21 @@
 ;(check-expect (sum-list (list  (list 3 4) 1 2) doubler) 20)
 ;(check-expect (sum-list (list 6 (list 3 (list 4)) 1 (list 1 2 (list 4 (list 1 3 (list (list 1 2) 6)) 2))) doubler) 72)
 
-(define b 12)
-(define a 13)
+;(define b 12)
+;(define a 13)
 
-(assert (< b a))
+;(let (a (amb (list 1 2 3 4 5 6 7)))
+;      (b (amb (list 1 2 3 4 5 6 7)))
+;      (c (amb (list 1 2 3 4 5 6 7))))
 
+
+
+(display
+  (call/cc (lambda (cc)
+            (display "I got here.\n")
+            (cc "This string was passed to the continuation.\n")
+            (display "But not here.\n"))))
+
+(let ([a 13]
+      [b 12])
+(assert (< b a)))
